bfcf588c115046793478ea819d28638f077163c0
Abinesh <abinesh.lal@gramenerit.com>
2016-11-15 22:17:11 +0530

    Dev: Database details updated
    Signed-off-by: Abinesh <abinesh.lal@gramenerit.com>

--- app/cache/prod/appParameters.php
+++ app/cache/prod/appParameters.php
@@ -1,11 +1,11 @@
 <?php return array (
   'parameters' => 
   array (
-    'database_host' => '127.0.0.1',
-    'database_port' => '',
-    'database_name' => 'prestashop',
-    'database_user' => 'root',
-    'database_password' => '',
+    'database_host' => '127.11.179.2',
+    'database_port' => '3306',
+    'database_name' => 'egarden',
+    'database_user' => 'adminBE8lzDS',
+    'database_password' => 'AMAWLgEfJejV',
     'database_prefix' => 'ps_',
     'database_engine' => 'InnoDB',
     'mailer_transport' => 'smtp',
